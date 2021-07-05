8b7f6ecd4d9b9e4f56a363ffe91c135ad33e31e0
zemichaelmd <zemichaelmd@gmail.com>
2021-07-05 12:43:22 +0300

    First commit - Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavBarContainer from "./components/nav-bar-container";
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBarContainer />
+      <div>this is a test</div>
+    </div>
+  );
+}
+
+export default App;
