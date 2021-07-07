baac3284c8a53974d5978aa2be5f60f79a34142c
zemichaelmd <zemichaelmd@gmail.com>
2021-07-07 23:43:24 +0300

    Setup react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/auth/login";
 import NavBarContainer from "./components/nav-bar-container";
 
 
 function App() {
   return (
-    <div className="App">
-      <NavBarContainer />
-      </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact component = {NavBarContainer}/>
+        <Route path='/login' exact component = {Login}/>
+      </Switch>
+    </Router>
   );
 }
 
