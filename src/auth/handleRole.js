3995a2f7dec9b3323ef5d5e1e0e5525377f4f23c
zemichaelmd <zemichaelmd@gmail.com>
2021-07-22 23:08:52 +0300

    FIx role management

--- src/auth/handleRole.js
+++ src/auth/handleRole.js
@@ -1,46 +1,106 @@
 import { Container } from "@material-ui/core";
 import { useState, useEffect } from "react";
-import { useHistory  } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 import AdminContainer from "../components/admin/admin-container";
 import ClerkContainer from "../components/clerk/clerk-container";
 import CompanyAdminContainer from "../components/company-admin/company-admin-container";
 import AuthService from "./Auth";
 
-export default function HandleRole() {
-  const history = useHistory()
-  const [role, setRole] = useState("");
 
+
+
+export default function HandleRole() {
+
+  const [role, setRole] = useState("");
+  const SetRole = () => {
+    AuthService.checkCachedJwtRole() === "admin"
+    ? setRole("admin")
+    : AuthService.checkCachedJwtRole() === "companyAdmin"
+    ? setRole("companyAdmin")
+    : AuthService.checkCachedJwtRole() === "clerk"
+    ? setRole("clerk")
+    : setRole("unknown");
+  }
+
+  const history = useHistory();
   useEffect(() => {
     //check the cachedJwt for role
-    if (AuthService.getCachedJwt() || AuthService.checkCachedJwtStatus() === 'OKAY') {
-      AuthService.checkCachedJwtRole() === "admin"
-        ? setRole("admin")
-        : AuthService.checkCachedJwtRole() === "companyAdmin"
-          ? setRole("companyAdmin")
-          : AuthService.checkCachedJwtRole() === "clerk"
-            ? setRole("clerk")
-            : setRole("unknown");
+    if (AuthService.getCachedJwt()) {
+      if (AuthService.checkCachedJwtStatus() === "OKAY") {
+        SetRole();
+      } else if (AuthService.checkCachedJwtStatus() === "REFRESH") {
+        AuthService.refreshToken(AuthService.getCachedJwt().accessToken);
+        SetRole();
+      } else if (AuthService.checkCachedJwtStatus() === "EXPIRED") {
+        return (
+          <Container
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              maxWidth: "400px",
+            }}
+          >
+            <h1 style={{ textAlign: "center" }}>
+              Session is Expired! Please log back in.
+            </h1>
+            <button
+              onClick={() => {
+                history.push("/login");
+              }}
+            >
+              {" "}
+              Login{" "}
+            </button>
+          </Container>
+        );
+      }
     }
     return () => {
       //cleanup
-    }
-  }, [])
+    };
+  }, []);
 
   if (role === "admin") {
     return <AdminContainer />;
   } else if (role === "clerk") {
     return <ClerkContainer />;
-  }
-   else if (role === "companyAdmin") {
+  } else if (role === "companyAdmin") {
     return <CompanyAdminContainer />;
-  }
-  else {
-    return <Container style={{ display: 'flex', flexDirection: 'column', maxWidth: "400px"}}><h1 style={{ textAlign: "center" }}>
-      No Role or Not logged in!
+  } else if (role === "unknown") {
+    return (
+      <Container
+        style={{ display: "flex", flexDirection: "column", maxWidth: "400px" }}
+      >
+        <h1 style={{ textAlign: "center" }}>
+          The user does not seem to have a role! Please login with a different
+          user!
+        </h1>
+        <button
+          onClick={() => {
+            history.push("/login");
+          }}
+        >
+          {" "}
+          Login{" "}
+        </button>
+      </Container>
+    );
+  } else {
+    return  <Container
+    style={{ display: "flex", flexDirection: "column", maxWidth: "400px" }}
+  >
+    <h1 style={{ textAlign: "center" }}>
+     Please Log in!
     </h1>
-      <button onClick = {() =>{history.push('/login')}}> Login </button>
-    </Container>
+    <button
+      onClick={() => {
+        history.push("/login");
+      }}
+    >
+      {" "}
+      Login{" "}
+    </button>
+  </Container>
   }
-
 }
