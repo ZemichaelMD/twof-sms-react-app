1bb9afe1a1b4abc672cd273749bb5ac5feff45f7
zemichaelmd <zemichaelmd@gmail.com>
2021-07-28 19:16:21 +0300

    Simplified Api calling

--- src/api-services/api.js
+++ src/api-services/api.js
@@ -1,12 +1,10 @@
 import axios from "axios";
-import { AuthService } from "../auth/Auth";
-
 
 const API_URL = "https://138.68.163.236/api/v1";
 const LOGIN_URL = API_URL + "/users/login";
-const REFRESH_URL = API_URL + "users/refreshToken";
-const REGISTER_URL = API_URL + "signup";
-const COMPANIES_GET_ALL = API_URL +'/companies'
+const REFRESH_URL = API_URL + "/users/refreshToken";
+const REGISTER_URL = API_URL + "/signup";
+const COMPANIES_GET_ALL = API_URL + '/companies'
 const BearerHeader = (token) => {
   let config = {
     headers: {
@@ -29,16 +27,9 @@ export async function Login(username, password) {
 }
 
 export async function refreshToken(token) {
-  let jwt = AuthService.getCachedJwt();
-  AuthService.removeCachedJwt();
-  let newToken;
-  axios.post(REFRESH_URL, BearerHeader(token)).then((res) => {
-    newToken = res
-  })
-  jwt = jwt.accessToken = newToken;
-  AuthService.saveJwt(jwt)
-  return jwt
+  return await axios.post(REFRESH_URL, BearerHeader(token)).then((response) => { return response })
 }
+
 
 export async function register(username, email, password) {
   return axios.post(REGISTER_URL, {
@@ -49,6 +40,7 @@ export async function register(username, email, password) {
 }
 
 export async function getAllCompanies(token) {
-   const data = await axios.get(COMPANIES_GET_ALL, BearerHeader(token));
-   return data;
-  }
+  const data = await axios.get(COMPANIES_GET_ALL, BearerHeader(token));
+  console.log(data);
+  return data;
+}
